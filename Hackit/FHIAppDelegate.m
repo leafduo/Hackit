6b57d2d4a61d4150449e9537a807d8c651a85175
Zuyang Kou <leafduo@gmail.com>
2013-08-13 01:50:39 +0800

    Compile clean

--- Hackit/FHIAppDelegate.m
+++ Hackit/FHIAppDelegate.m
@@ -15,7 +15,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    FHIHomeTableViewController *homeTableViewController = (FHIHomeTableViewController *)self.window.rootViewController;
 
     return YES;
 }
